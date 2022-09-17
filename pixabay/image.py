23e9f805c47a21a9322627a2470b62bfe3254ebd
Lukáš Plevač <lukasplevac@gmail.com>
2022-09-17 10:45:17 +0200

    Fixed function call getPreviewURL

--- pixabay/image.py
+++ pixabay/image.py
@@ -158,7 +158,7 @@ class image:
     # Get published date of image
     # @return datetime UTC of image publication
     def getPublishedDate(self):
-        preview_url = self.getPreviewUrl()
+        preview_url = self.getPreviewURL()
         
         #  year / month / day / hour / minute
         match = re.search('\d{4}/\d{2}/\d{2}/\d{2}/\d{2}', preview_url)
